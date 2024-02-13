69f396c63b3040f70d64bc5b2d25e6e6432f4a49
KlausMikhaelson <satyamsingh5076@gmail.com>
2024-02-13 15:04:36 -0600

    updated aboutjs content

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -10,19 +10,15 @@ const About = () => {
       <div className="flex items-center justify-center border-[3px] border-green-500 p-1">
         <div className="flex items-center justify-center border-[3px] border-green-400 p-4">
           <h1 className="text-green-400 text-xl">
-            In the fall semester of 2023 our president i.e. Andrew petre also known as Ruswal on discord
-            registered this club in order to fill in the gap of the actual
-            experience that we need in order to get into cyber security and what
-            we were learning at uofr, he started this club with our operation
-            lead i.e. Raj also known as Flux on discord
+          In the fall semester of 2023 our president, known as Ruswal on discord, registered this club in order to close the gap of the actual experience that we need to go from entry level noobs to formidable and knowledgeable security specialists and the right steps we should take in order to achieve our pursuits in this industry. He started this club with our operation lead, known as Flux on discord.
             {'\n'}
             <p>
-                For a semester we operated via discord, built a small community of 100+ students, gradually some really experienced hackers from the regina community joined us and started helping people to get into cyber security with the help of their own experience.
-            </p>
+            For a semester we operated via discord, built a small community of 100+ students, gradually some really experienced experts, hackers, engineers, and teachers from the Regina community joined us and started helping people to get into cyber security guided by the knowledge of their own experience.
+                </p>
             <p>
-                Fast forward to 2024 winter semester we participated in University of Regina's club fair and introduced new and existing students about our club and onboarded them as well.
-            </p>
-            <p>Now in the first week of February we have planed to host our first in-person event about which we wil be psoting infromation shortly so stay tuned!</p>
+            Fast forward to 2024 winter semester we displayed what we had to offer when we participated in University of Regina's club fair and introduced new and existing students to what our club was about and onboarded them as well.
+                </p>
+            <p>Now in the coming weeks we have planed to host our first in-person event to really get personal with the students and share our resources with them. regarding the event, we will be posting information shortly so stay tuned!</p>
             <Typewriter
               onInit={(typewriter) => {
                 typewriter
