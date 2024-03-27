0e3c5c67e4c2df2df9a85685932684ec64835408
KlausMikhaelson <satyamsingh5076@gmail.com>
2024-03-26 19:42:54 -0600

    fixed ui

--- client/src/components/evervault-card.js
+++ client/src/components/evervault-card.js
@@ -45,9 +45,9 @@ export const EvervaultCard = ({
         />
         <div className="relative z-10 flex items-center justify-center">
           <div className="relative h-44 w-44  rounded-full flex items-center justify-center text-white font-bold text-4xl">
-            <div className="absolute w-full h-full bg-white/[0.8] dark:bg-black/[0.4] blur-sm rounded-full" />
+            <div className="absolute bg-white/[0.8] dark:bg-black/[0.4] blur-sm rounded-full" />
             {/* <span className="dark:text-white text-black z-20">{text}</span> */}
-            <img className="w-45 h-45 rounded-full" src={text} alt="Person" />
+            <img className="w-44 h-44 rounded-full" src={text} alt="Person" />
           </div>
         </div>
       </div>
